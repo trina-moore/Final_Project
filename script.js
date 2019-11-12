7c4811a663c13021a48861640543d17e9950aa7d
Trina Moore <[trinamoore91@gmail.com]>
2019-11-12 02:14:30 -0600

    Trina Moore
    made additional changes to the files

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var button = document.getElementById("enter");
+var enter = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var today = new Date();
+var theTime = today.getHours();
+var greeting;
 
 function inputLength(){
   return input.value.length;
@@ -11,6 +14,18 @@ function createListElement(){
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
   input.value = "";
+}
+
+function removeListElement(){
+  var item = document.getElementById(enter.value);
+  ul.removeChild(item);
+}
+
+function removeItem(itemid){
+   alert("You're going to delete all your items!");
+ while (ul.hasChildNodes()) {
+   ul.removeChild(ul.firstChild);
+ }
 }
 
 function addListAfterClick(){
@@ -25,5 +40,7 @@ function addListAfterKeypress(event){
   }
 }
 
-button.addEventListener("click", addListAfterClick);
+enter.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
+
+// Displays time of day
