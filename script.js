ac197a7d8b565337385c0addff348ec84703d3b5
Trina Moore <[trinamoore91@gmail.com]>
2019-12-06 10:02:35 -0600

    added try/catch statement
    added try/catch statement

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var enter = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var error = document.getElementById("errormessage");
 
-
-function inputLength(){
+function inputLength() {
   return input.value.length;
 }
 
 // adds list items and appends
-function createListElement(){
+function createListElement() {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
@@ -16,22 +16,30 @@ function createListElement(){
 }
 
 // displays window alert message and removes all list items
-function removeItem(itemid){
-   alert("You're going to delete all your items!");
- while (ul.hasChildNodes()) {
-   ul.removeChild(ul.firstChild);
- }
+function removeItem(itemid) {
+  alert("You're going to delete all your items!");
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
 }
 
 // adds list items after pressing enter button
-function addListAfterClick(){
+function addListAfterClick() {
+
+  error.innerHTML = "";
+  try {
+    if (inputLength() = 0) throw "input is invalid";
+  } catch (err) {
+    error.innerHTML = "Input is invalid";
+  }
   if (inputLength() > 0) {
     createListElement();
   }
 }
 
 // adds list items after clicking enter key
-function addListAfterKeypress(event){
+function addListAfterKeypress(event) {
+  error.innerHTML = "";
   if (inputLength() > 0 && event.keyCode === 13) {
     createListElement();
   }
